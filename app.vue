95d00dffdd469c39445a8e28bc7117ab76df714d
serhii <loveykosergey@ukr.net>
2025-05-14 20:44:54 +0300

    feat: add section hero

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <h1>The Infin</h1>
-  </div>
+  <AppHero />
+  <!-- <AppVideo /> -->
+  <!-- <AppBusines /> -->
+  <!-- <AppIndividuals /> -->
 </template>
+
+
---
+++ components/AppHero.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="hero wrapper">
+    <AppHeader />
+    <AppHeroMobile />
+    <AppHeroContribution />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+  .hero {
+    padding: 30px 18px;
+  }
+
+</style>
