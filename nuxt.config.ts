221c057f9fb07c3b230e26c1e5c79287c3c63e82
serhii <loveykosergey@ukr.net>
2025-06-10 22:52:37 +0300

    feat: add configuration setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
+  app: {
+    baseURL: '/',
+  },
   css: ['/assets/scss/main.scss']
 })
