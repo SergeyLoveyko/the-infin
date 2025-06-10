aaebd7e1597acb40a22d9351821a1cae70485262
serhii <loveykosergey@ukr.net>
2025-06-11 01:25:28 +0300

    fix: path deploy project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   app: {
-    baseURL: '/',
+    baseURL: '/the-infin/',
   },
   css: ['/assets/scss/main.scss']
 })
