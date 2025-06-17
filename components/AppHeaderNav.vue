4cee76d669afdf945ee1b04a6c49d10c30860aad
serhii <loveykosergey@ukr.net>
2025-06-18 02:43:50 +0300

    [SITE-2] rewrote nav via v-for

--- components/AppHeaderNav.vue
+++ components/AppHeaderNav.vue
@@ -1,19 +1,43 @@
+<script setup>
+const items = [
+  'Home',
+  'For Businesses',
+  'For Individuals',
+  'Capitalism 2.0',
+  'Marketing Efforts'
+  ]
+</script>
+
 <template>
   <nav class="header__nav nav__header">
-      <ul class="nav__header-list">
-        <AppHeaderNavItem>Home</AppHeaderNavItem>
-        <AppHeaderNavItem>For Businesses</AppHeaderNavItem>
-        <AppHeaderNavItem>For Individuals</AppHeaderNavItem>
-        <AppHeaderNavItem>Capitalism 2.0</AppHeaderNavItem>
-        <AppHeaderNavItem>Marketing Efforts</AppHeaderNavItem>
-      </ul>
-    </nav>
+    <ul class="nav__header-list">
+      <li class="nav__header-item" v-for="item in items" :key="item">
+        <a class="nav__header-link" href="#">
+          {{ item }}
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
 
   .nav__header-list {
     list-style: none; 
+  }
+
+  .nav__header-item {
+    line-height: 1.3;
+  }
+
+  .nav__header-link {
+    text-decoration: none;
+    color: #121212;
+    transition: all 0.2s;
+
+    &:hover {
+      color: #8f8f8f;
+    }
   }
 
 </style>
